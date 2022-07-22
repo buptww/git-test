4921c4b7345c044d297c76f375ae9f03caf0b3d0
buptww <52535185+buptww@users.noreply.github.com>
2022-07-22 09:02:02 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("1");
         System.out.println("hot-fix");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
